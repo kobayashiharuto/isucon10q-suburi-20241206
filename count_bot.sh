b5ccaa755758b577b3e1f7c6956ac493e2579699
kobayashiharuto <datesaver1234@gmail.com>
2024-12-06 23:42:08 +0900

    時間指定可能に

--- count_bot.sh
+++ count_bot.sh
@@ -3,20 +3,32 @@
 # ログファイルのパス
 LOG_FILE="/var/log/nginx/access.log"
 
-# 正規表現パターン
-BOT_PATTERNS='ISUCONbot(-Mobile)?|ISUCONbot-Image\/|Mediapartners-ISUCON|ISUCONCoffee|ISUCONFeedSeeker(Beta)?|crawler \(https:\/\/isucon\.invalid\/(support\/faq\/|help\/jp\/)|isubot|Isupider|Isupider(-image)?\+|(bot|crawler|spider)(?:[-_ .\/;@()]|$)'
-
-# 14:20以降のログをフィルタリング
-MATCHED_LINES=$(awk '$4 ~ /06\/Dec\/2024:14:2[0-9]:|06\/Dec\/2024:14:[3-5][0-9]:|06\/Dec\/2024:15:/' "$LOG_FILE" | grep -E "$BOT_PATTERNS" | wc -l)
-TOTAL_LINES=$(awk '$4 ~ /06\/Dec\/2024:14:2[0-9]:|06\/Dec\/2024:14:[3-5][0-9]:|06\/Dec\/2024:15:/' "$LOG_FILE" | wc -l)
-
-# 結果を計算
-if [ $TOTAL_LINES -gt 0 ]; then
-    BOT_PERCENTAGE=$(echo "scale=2; ($MATCHED_LINES / $TOTAL_LINES) * 100" | bc)
-    echo "Total requests (14:20 and later): $TOTAL_LINES"
-    echo "Bot requests (14:20 and later): $MATCHED_LINES"
-    echo "Bot request percentage: $BOT_PERCENTAGE%"
-else
-    echo "No requests found after 14:20."
+# 時間の指定 (引数)
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 HH:MM"
+    exit 1
 fi
 
+TARGET_TIME="$1"
+
+# 正規表現リストを OR (|) でつなげたパターン
+BOT_PATTERNS='ISUCONbot(-Mobile)?|ISUCONbot-Image\/|Mediapartners-ISUCON|ISUCONCoffee|ISUCONFeedSeeker(Beta)?|crawler \(https:\/\/isucon\.invalid\/(support\/faq\/|help\/jp\/)|isubot|Isupider|Isupider(-image)?\+|(bot|crawler|spider)(?:[-_ .\/;@()]|$)'
+
+# 指定された時間以降のログを抽出
+TARGET_LOGS=$(awk -v time="$TARGET_TIME" '$0 ~ time {print $0}' "$LOG_FILE")
+
+# 指定時間以降のリクエスト総数
+TOTAL_REQUESTS=$(echo "$TARGET_LOGS" | wc -l)
+
+# 指定時間以降のボットリクエスト数をカウント
+BOT_REQUESTS=$(echo "$TARGET_LOGS" | grep -E "$BOT_PATTERNS" | wc -l)
+
+# 割合計算
+if [ "$TOTAL_REQUESTS" -eq 0 ]; then
+    echo "No requests found after $TARGET_TIME."
+else
+    BOT_PERCENTAGE=$(echo "scale=2; ($BOT_REQUESTS / $TOTAL_REQUESTS) * 100" | bc)
+    echo "Total requests after $TARGET_TIME: $TOTAL_REQUESTS"
+    echo "Bot requests after $TARGET_TIME: $BOT_REQUESTS"
+    echo "Bot request percentage: $BOT_PERCENTAGE%"
+fi
