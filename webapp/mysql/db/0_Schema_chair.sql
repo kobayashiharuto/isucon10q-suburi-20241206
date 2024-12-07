309152019fe338bd55c4c047479fda43d4a94284
kobayashiharuto <datesaver1234@gmail.com>
2024-12-08 00:17:55 +0900

    a

--- webapp/mysql/db/0_Schema_chair.sql
+++ webapp/mysql/db/0_Schema_chair.sql
@@ -1,6 +1,7 @@
 DROP DATABASE IF EXISTS isuumo;
 CREATE DATABASE isuumo;
 
+DROP TABLE IF EXISTS isuumo.estate;
 DROP TABLE IF EXISTS isuumo.chair;
 
 CREATE TABLE isuumo.chair
@@ -18,6 +19,26 @@ CREATE TABLE isuumo.chair
     kind        VARCHAR(64)     NOT NULL,
     popularity  INTEGER         NOT NULL,
     stock       INTEGER         NOT NULL
+);
+
+
+CREATE TABLE isuumo.estate
+(
+    id          INTEGER             NOT NULL PRIMARY KEY,
+    name        VARCHAR(64)         NOT NULL,
+    description VARCHAR(4096)       NOT NULL,
+    thumbnail   VARCHAR(128)        NOT NULL,
+    address     VARCHAR(128)        NOT NULL,
+    latitude    DOUBLE PRECISION    NOT NULL,
+    longitude   DOUBLE PRECISION    NOT NULL,
+    rent        INTEGER             NOT NULL,
+    door_height INTEGER             NOT NULL,
+    door_width  INTEGER             NOT NULL,
+    features    VARCHAR(64)         NOT NULL,
+    popularity  INTEGER             NOT NULL,
+    location    POINT GENERATED ALWAYS AS (Point(longitude, latitude)) STORED NOT NULL,
+    INDEX idx_popularity (popularity),
+    SPATIAL INDEX idx_location (location)
 );
 
 
