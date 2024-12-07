5c20b6cf92d52a6127e4f4c16f1c8ed63bb19532
kobayashiharuto <datesaver1234@gmail.com>
2024-12-07 16:30:19 +0900

    id, ASC を消した

--- webapp/nodejs/app.js
+++ webapp/nodejs/app.js
@@ -602,7 +602,7 @@ app.get("/api/recommended_estate/:id", async (req, res, next) => {
       WHERE 
         (door_width >= ? AND door_height >= ?) OR 
         (door_width >= ? AND door_height >= ?)
-      ORDER BY popularity DESC, id ASC 
+      ORDER BY popularity DESC
       LIMIT ?`,
       [min1, min2, min2, min1, LIMIT]
     );
